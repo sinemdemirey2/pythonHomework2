847b60f9bfe82d88f8f89d21fa3e148eb5d3a975
sinemdemirey2 <sinem.demirey@useinsider.com>
2020-10-04 16:57:35 +0300

    exercise 3 done

--- exercise3.py
+++ exercise3.py
@@ -1,3 +1,11 @@
+"""
+Define a class called Lunch.Its __init__() method should have two arguments:selfanf menu.Where menu is a string.
+
+Add a method called menu_price.It will involve a ifstatement:
+
+if "menu 1" print "Your choice:", menu, "Price 12.00", if "menu 2" print "Your choice:", menu, "Price 13.40", else print "Error in menu".
+To check if it works define: Paul=Lunch("menu 1") and call Paul.menu_price().
+"""
 class Lunch:
     def __init__(self,menu):
         self.menu = menu
