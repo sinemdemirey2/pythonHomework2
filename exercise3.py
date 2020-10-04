c2db455ac2d125269c1f230da31f1cd5bc53029b
sinemdemirey2 <sinem.demirey@useinsider.com>
2020-10-04 16:31:04 +0300

    exercise 3 done

---
+++ exercise3.py
@@ -0,0 +1,13 @@
+class Lunch:
+    def __init__(self,menu):
+        self.menu = menu
+    def menu_price(self):
+        if self.menu == "menu 1":
+            print("Your choice:", self.menu, "Price 12.00$")
+        elif self.menu == "menu 2":
+            print("Your choice:", self.menu, "Price 13.40$")
+        else:
+            print("Error in menu.")
+
+paul = Lunch("menu 2")
+paul.menu_price()
