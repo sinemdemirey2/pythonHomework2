23608160d0474de1c00a37d79194dcc62e303b9f
sinemdemirey2 <sinem.demirey@useinsider.com>
2020-10-04 16:21:01 +0300

    exercise 2 done

--- exercise1.py
+++ exercise1.py
@@ -1,5 +1,20 @@
-#I am tryin to push this to github
+class Triangle:
+    number_of_sides = 3
 
-print("Hello world")
+    def __init__(self,angle1,angle2,angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
 
-print("Lets try pushing changes")
+    def check_angles(self):
+        sum = self.angle1 + self.angle2 + self.angle3
+
+        if sum == 180:
+            return True
+        else:
+            return False
+
+my_triangle = Triangle(90,10,60)
+
+print(my_triangle.number_of_sides)
+print(my_triangle.check_angles())
